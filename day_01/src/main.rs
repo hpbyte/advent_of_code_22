4b5ccdd2c51909e42e4f5db0c4f49c8e76cd17fb
Htoo Pyae <htoopyae.dev@gmail.com>
2022-12-05 14:30:50 +0700

    day 01 part 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("./test.input") {
+        let mut prev_max = 0;
+        let mut sum = 0;
+        for line in lines {
+            if let Ok(str) = line {
+                if str.is_empty() {
+                    if sum > prev_max {
+                        prev_max = sum;
+                    }
+                    sum = 0;
+                } else {
+                    match str.parse::<i32>() {
+                        Ok(num) => {
+                            sum = sum + num;
+                        },
+                        Err(_e) => {
+                            panic!("cannot convert to an integer");
+                        }
+                    }
+                }
+            }
+        }
+        println!("The most calories being carried is {}", prev_max);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
