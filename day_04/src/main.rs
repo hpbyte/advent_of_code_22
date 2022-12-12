66b0ad2cfbe52e28784400fac946b92b36065956
Htoo Pyae <htoopyae.dev@gmail.com>
2022-12-12 10:41:40 +0700

    day_04 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     }
 
     if let Some(ans) = process_part_2(filename) {
-        println!("Part 2 : The total: {}", ans);
+        println!("Part 2 : The total number of assignment pairs: {}", ans);
     }
 }
 
@@ -30,6 +30,6 @@ mod tests {
     #[test]
     fn part_2_should_work() {
         let ans = process_part_2("./sample.input").unwrap();
-        assert_eq!(ans, 0);
+        assert_eq!(ans, 4);
     }
 }
--- day_04/src/soln.rs
+++ day_04/src/soln.rs
@@ -1,7 +1,5 @@
-#![allow(unused)]
-
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -12,28 +10,33 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+fn process_lines(lines: Lines<BufReader<File>>) -> Vec<Vec<i32>> {
+    let line_pairs = lines
+        .filter(|l| l.is_ok() && !l.as_ref().unwrap().is_empty())
+        .map(|l| {
+            let pairs: Vec<i32> = l
+                .unwrap()
+                .split(',')
+                .collect::<Vec<&str>>()
+                .iter()
+                .flat_map(|s| s.split('-'))
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>();
+
+            debug_assert!(pairs.len() == 4);
+
+            return pairs;
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    line_pairs
+}
+
 pub fn process_part_1(filename: &str) -> Option<usize> {
     if let Ok(lines) = read_lines(filename) {
-        let total_fully_contained_pairs = lines
-            .filter(|l| l.is_ok() && !l.as_ref().unwrap().is_empty())
-            .filter(|l| {
-                if let Ok(line) = l {
-                    let pairs: Vec<i32> = line
-                        .split(',')
-                        .collect::<Vec<&str>>()
-                        .iter()
-                        .flat_map(|s| s.split('-'))
-                        .map(|s| s.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>();
-
-                    debug_assert!(pairs.len() == 4);
-
-                    return (pairs[0] <= pairs[2] && pairs[1] >= pairs[3])
-                        || (pairs[2] <= pairs[0] && pairs[3] >= pairs[1]);
-                }
-
-                false
-            })
+        let total_fully_contained_pairs = process_lines(lines)
+            .iter()
+            .filter(|v| (v[0] <= v[2] && v[1] >= v[3]) || (v[2] <= v[0] && v[3] >= v[1]))
             .count();
 
         return Some(total_fully_contained_pairs);
@@ -42,9 +45,17 @@ pub fn process_part_1(filename: &str) -> Option<usize> {
     None
 }
 
-pub fn process_part_2(filename: &str) -> Option<i32> {
+pub fn process_part_2(filename: &str) -> Option<usize> {
     if let Ok(lines) = read_lines(filename) {
-        return Some(0);
+        let total_fully_contained_pairs = process_lines(lines)
+            .iter()
+            .filter(|v| {
+                (v[0] <= v[2] && (v[1] >= v[3] || v[1] >= v[2]))
+                    || (v[2] <= v[0] && (v[3] >= v[1] || v[3] >= v[0]))
+            })
+            .count();
+
+        return Some(total_fully_contained_pairs);
     }
 
     None
