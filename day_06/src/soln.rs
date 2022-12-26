f11b7bc67cef249b75316ded2bd5fcdc87724138
Htoo Pyae <htoopyae.dev@gmail.com>
2022-12-26 14:51:06 +0700

    day_06 part 1

---
+++ day_06/src/soln.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+
+pub fn process_part_1(filename: &str) -> &str {
+    ""
+}
+
+pub fn process_part_2(filename: &str) -> &str {
+    ""
+}
+
